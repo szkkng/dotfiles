4334eb9918b7de7227fabaa7878fca23d552b9df
szkkng <zukkesukke23@gmail.com>
2022-11-13 00:36:43 +0900

    Change to tokyonight-moon theme

--- tools/wezterm/.config/wezterm/wezterm.lua
+++ tools/wezterm/.config/wezterm/wezterm.lua
@@ -2,14 +2,14 @@ local wezterm = require 'wezterm';
 
 return {
 
-  color_scheme = "iceberg-dark",
+  color_scheme = "tokyonight",
 
   font = wezterm.font ("Fira Code"),
   font_size = 14,
 
   tab_bar_at_bottom = true,
 
-  window_background_opacity = 0.95,
+  window_background_opacity = 0.85,
   window_decorations = "RESIZE",
   enable_tab_bar = false,
   default_prog = { '/bin/zsh', '-l', '-c', "tmux a -t 0 || tmux" },
