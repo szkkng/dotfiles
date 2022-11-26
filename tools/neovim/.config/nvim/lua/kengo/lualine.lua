efd48d6440883e05efd26f575befb97f15fc0270
szkkng <zukkesukke23@gmail.com>
2022-11-26 22:12:44 +0900

    Change to kanagawa & iceberg-dark scheme

--- tools/neovim/.config/nvim/lua/kengo/lualine.lua
+++ tools/neovim/.config/nvim/lua/kengo/lualine.lua
@@ -1,4 +1,15 @@
+local custom_iceberg = require'lualine.themes.iceberg_dark'
+
+custom_iceberg.normal.c.bg = nil
+-- custom_iceberg.inactive.a.bg = nil
+-- custom_iceberg.inactive.b.bg = nil
+custom_iceberg.inactive.c.bg = nil
+-- custom_iceberg.normal.b.bg = "1f1f28"
+
 require('lualine').setup {
+  options = {
+    theme = custom_iceberg
+  },
   tabline = {
     lualine_a = {
       {
