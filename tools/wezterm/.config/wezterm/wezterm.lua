1073bb8a396a03065ecb5a108078661b9ec52353
Kengo Suzuki <git@kengo.dev>
2023-06-10 12:49:02 +0900

    Update wezterm config

--- tools/wezterm/.config/wezterm/wezterm.lua
+++ tools/wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,72 @@
 local wezterm = require 'wezterm';
 
+local SOLID_LEFT_ARROW = utf8.char(0x2590)
+local SOLID_RIGHT_ARROW = utf8.char(0x258c)
+
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+
+  if title and #title > 0 then
+    return title
+  end
+
+  return tab_info.active_pane.title
+end
+
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    local background = '#1f1f28'
+    local foreground = '#727169'
+
+    if tab.is_active then
+      foreground = '#dcd7ba'
+      background = '#2a2a37'
+    end
+
+    local edge_foreground = background
+    local edge_background = background
+
+    local title = tab_title(tab)
+
+    -- ensure that the titles fit in the available space,
+    -- and that we have room for the edges.
+    title = wezterm.truncate_right(title, max_width - 2)
+
+    return {
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_LEFT_ARROW },
+      { Background = { Color = background } },
+      { Foreground = { Color = foreground } },
+      { Text = title },
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_RIGHT_ARROW },
+    }
+  end
+)
+
 return {
+  default_prog = { '/opt/homebrew/bin/fish', '-l' },
   font = wezterm.font ("FiraCode Nerd Font"),
-  font_size = 14,
+  font_size = 12,
+  line_height = 1.2,
 
-  tab_bar_at_bottom = true,
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  show_new_tab_button_in_tab_bar = false,
+  tab_max_width = 50,
 
-  window_background_opacity = 0.92,
+  window_close_confirmation = "NeverPrompt",
+  window_background_opacity = 0.9,
   window_decorations = "RESIZE",
-  enable_tab_bar = false,
-  default_prog = { '/opt/homebrew/bin/fish', '-l', '-c', "tmux a -t 0 || tmux" },
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
 
   -- Copyright (c) 2021 Tommaso Laurenzi
   -- Released under the MIT license.
@@ -25,6 +82,10 @@ return {
 
     selection_fg = "#c8c093",
     selection_bg = "#2d4f67",
+
+    tab_bar = {
+      background = '#1f1f28'
+    },
 
     scrollbar_thumb = "#16161d",
     split = "#16161d",
