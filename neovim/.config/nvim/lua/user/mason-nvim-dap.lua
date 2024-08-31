886976b5566c7a0d2f416dfb1d8826d2431f0cd5
Kengo Suzuki <git@kengo.dev>
2024-09-01 00:55:12 +0900

    Clean up dap settings

--- neovim/.config/nvim/lua/user/mason-nvim-dap.lua
+++ neovim/.config/nvim/lua/user/mason-nvim-dap.lua
@@ -1,17 +1,14 @@
-local M = {
+return {
   "jay-babu/mason-nvim-dap.nvim",
   event = "VeryLazy",
   dependencies = {
     "williamboman/mason.nvim",
     "mfussenegger/nvim-dap",
   },
+  config = function()
+    require("mason-nvim-dap").setup {
+      ensure_installed = { "codelldb" },
+      handlers = {},
+    }
+  end,
 }
-
-function M.config()
-  require("mason-nvim-dap").setup {
-    ensure_installed = { "codelldb" },
-    handlers = {},
-  }
-end
-
-return M
