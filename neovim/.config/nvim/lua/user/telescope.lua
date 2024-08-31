f0d8dbd77963d56173b89b3f0db51bea118b5ece
szkkng <hi@kengo.dev>
2024-08-31 18:29:05 +0900

    Clean up telescope setting

--- neovim/.config/nvim/lua/user/telescope.lua
+++ neovim/.config/nvim/lua/user/telescope.lua
@@ -1,33 +1,25 @@
 local M = {
   "nvim-telescope/telescope.nvim",
-  dependencies = { { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true } },
+  dependencies = {
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+      lazy = true,
+    },
+  },
 }
 
 function M.config()
-  local wk = require "which-key"
-  wk.register {
-    ["<leader>bb"] = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
-    ["<leader>fb"] = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    ["<leader>fc"] = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-    ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find files" },
-    ["<leader>ft"] = { "<cmd>Telescope live_grep<cr>", "Find Text" },
-    ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Help" },
-    ["<leader>fl"] = { "<cmd>Telescope resume<cr>", "Last Search" },
-    ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
-  }
+  local builtin = require "telescope.builtin"
+  vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+  vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+  vim.keymap.set("n", "<leader>ft", builtin.live_grep, {})
+  vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
-  local icons = require "user.icons"
   local actions = require "telescope.actions"
 
   require("telescope").setup {
     defaults = {
-      prompt_prefix = icons.ui.Telescope .. " ",
-      selection_caret = icons.ui.Forward .. " ",
-      entry_prefix = "   ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      path_display = { "smart" },
-      color_devicons = true,
       vimgrep_arguments = {
         "rg",
         "--color=never",
@@ -39,38 +31,21 @@ function M.config()
         "--hidden",
         "--glob=!.git/",
       },
-
-      mappings = {
-        i = {
-          ["<C-n>"] = actions.move_selection_next,
-          ["<C-p>"] = actions.move_selection_previous,
-        },
-        n = {
-          ["<esc>"] = actions.close,
-          ["j"] = actions.move_selection_next,
-          ["k"] = actions.move_selection_previous,
-          ["q"] = actions.close,
-        },
-      },
     },
     pickers = {
       live_grep = {
         theme = "dropdown",
       },
-
       grep_string = {
         theme = "dropdown",
       },
-
       find_files = {
         theme = "dropdown",
         previewer = false,
       },
-
       buffers = {
         theme = "dropdown",
         previewer = false,
-        initial_mode = "normal",
         mappings = {
           i = {
             ["<C-d>"] = actions.delete_buffer,
@@ -80,45 +55,33 @@ function M.config()
           },
         },
       },
-
       planets = {
         show_pluto = true,
         show_moon = true,
       },
-
       colorscheme = {
         enable_preview = true,
       },
-
       lsp_references = {
         theme = "dropdown",
         initial_mode = "normal",
       },
-
       lsp_definitions = {
         theme = "dropdown",
         initial_mode = "normal",
       },
-
       lsp_declarations = {
         theme = "dropdown",
         initial_mode = "normal",
       },
-
       lsp_implementations = {
         theme = "dropdown",
         initial_mode = "normal",
       },
     },
-    extensions = {
-      fzf = {
-        fuzzy = true, -- false will only do exact matching
-        override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-      },
-    },
   }
+
+  require("telescope").load_extension "fzf"
 end
 
 return M
