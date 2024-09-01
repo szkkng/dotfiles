d45ce780234f7981e5204568c13b34be944d39ac
Kengo Suzuki <git@kengo.dev>
2024-09-01 10:48:25 +0900

    Clean up lspconfig

--- neovim/.config/nvim/lua/user/lspconfig.lua
+++ neovim/.config/nvim/lua/user/lspconfig.lua
@@ -1,4 +1,4 @@
-local M = {
+return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
@@ -6,120 +6,68 @@ local M = {
       "folke/neodev.nvim",
     },
   },
-}
-
-local function lsp_keymaps(bufnr)
-  local opts = { noremap = true, silent = true }
-  local keymap = vim.api.nvim_buf_set_keymap
-  keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-end
-
-M.on_attach = function(client, bufnr)
-  lsp_keymaps(bufnr)
-end
-
-function M.common_capabilities()
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-  return capabilities
-end
-
-function M.config()
-  -- local wk = require "which-key"
-  -- wk.register {
-  --   ["<leader>la"] = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-  --   ["<leader>lf"] = {
-  --     "<cmd>lua vim.lsp.buf.format({async = true, filter = function(client) return client.name ~= 'typescript-tools' end})<cr>",
-  --     "Format",
-  --   },
-  --   ["<leader>li"] = { "<cmd>LspInfo<cr>", "Info" },
-  --   ["<leader>lj"] = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic" },
-  --   ["<leader>lh"] = { "<cmd>lua require('user.lspconfig').toggle_inlay_hints()<cr>", "Hints" },
-  --   ["<leader>lk"] = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
-  --   ["<leader>ll"] = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-  --   ["<leader>lq"] = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
-  --   ["<leader>lr"] = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-  -- }
-  --
-  -- wk.register {
-  --   ["<leader>la"] = {
-  --     name = "LSP",
-  --     a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action", mode = "v" },
-  --   },
-  -- }
-
-  local lspconfig = require "lspconfig"
-  local icons = require "user.icons"
-
-  local servers = {
-    "clangd",
-    "lua_ls",
-    "cssls",
-    "html",
-    "tsserver",
-    "eslint",
-    "tsserver",
-    "eslint",
-    "bashls",
-    "jsonls",
-    "yamlls",
-  }
-
-  local default_diagnostic_config = {
-    signs = {
-      active = true,
-      values = {
-        { name = "DiagnosticSignError", text = icons.diagnostics.Error },
-        { name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-        { name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-        { name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
-      },
-    },
-    virtual_text = false,
-    update_in_insert = false,
-    underline = true,
-    severity_sort = true,
-    float = {
-      focusable = true,
-      style = "minimal",
-      border = "rounded",
-      source = "always",
-      header = "",
-      prefix = "",
-    },
-  }
-
-  vim.diagnostic.config(default_diagnostic_config)
-
-  for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
-  end
-
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-  require("lspconfig.ui.windows").default_options.border = "rounded"
-
-  for _, server in pairs(servers) do
-    local opts = {
-      on_attach = M.on_attach,
-      capabilities = M.common_capabilities(),
+  config = function()
+    local servers = {
+      "clangd",
+      "lua_ls",
+      "cssls",
+      "html",
+      "tsserver",
+      "eslint",
+      "tsserver",
+      "eslint",
+      "bashls",
+      "jsonls",
+      "yamlls",
     }
 
-    local require_ok, settings = pcall(require, "user.lspsettings." .. server)
-    if require_ok then
-      opts = vim.tbl_deep_extend("force", settings, opts)
+    local default_diagnostic_config = {
+      virtual_text = false,
+      update_in_insert = false,
+      severity_sort = true,
+    }
+
+    vim.diagnostic.config(default_diagnostic_config)
+
+    local function lsp_keymaps(bufnr)
+      local opts = { noremap = true, buffer = bufnr, silent = true }
+      vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "<leader>lq", vim.diagnostic.setloclist, opts)
+      vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
+      vim.keymap.set("n", "<leader>li", "<cmd>LspInfo<cr>", opts)
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
     end
 
-    if server == "lua_ls" then
-      require("neodev").setup {}
+    local function common_capabilities()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      return capabilities
     end
 
-    lspconfig[server].setup(opts)
-  end
-end
+    for _, server in pairs(servers) do
+      local opts = {
+        on_attach = function(_, bufnr)
+          lsp_keymaps(bufnr)
+        end,
+        capabilities = common_capabilities(),
+      }
 
-return M
+      local require_ok, settings = pcall(require, "user.lspsettings." .. server)
+      if require_ok then
+        opts = vim.tbl_deep_extend("force", settings, opts)
+      end
+
+      if server == "lua_ls" then
+        require("neodev").setup {}
+      end
+
+      require("lspconfig")[server].setup(opts)
+    end
+  end,
+}
