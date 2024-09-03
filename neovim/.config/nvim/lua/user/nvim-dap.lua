00a349fecbb6335414bec6fd009f1f5a4dfbcbcb
szkkng <hi@kengo.dev>
2024-09-03 17:41:44 +0900

    Update dap keymaps

--- neovim/.config/nvim/lua/user/nvim-dap.lua
+++ neovim/.config/nvim/lua/user/nvim-dap.lua
@@ -2,9 +2,13 @@ return {
   "mfussenegger/nvim-dap",
   event = "VeryLazy",
   keys = {
-    { "<leader>db", "<cmd>DapToggleBreakpoint<cr>" },
-    { "<leader>dr", "<cmd>DapContinue<cr>" },
-    { "<leader>dq", "<cmd>DapTerminate<cr>" },
+    { "<leader>db", "<cmd>DapToggleBreakpoint<CR>" },
+    { "<leader>dB", "<cmd>lua require('dap').clear_breakpoints()<CR>" },
+    { "<leader>dr", "<cmd>DapContinue<CR>" },
+    { "<leader>dq", "<cmd>DapTerminate<CR>" },
+    { "<leader>d1", "<cmd>DapStepOver<CR>" },
+    { "<leader>d2", "<cmd>DapStepInto<CR>" },
+    { "<leader>d3", "<cmd>DapStepOut<CR>" },
   },
   config = function() end,
 }
