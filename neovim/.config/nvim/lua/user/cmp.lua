17d2cfd3ca38b9c4c8c2961b4de856e047b0974b
Kengo Suzuki <git@kengo.dev>
2024-09-04 20:09:34 +0900

    Clean up configs

--- neovim/.config/nvim/lua/user/cmp.lua
+++ neovim/.config/nvim/lua/user/cmp.lua
@@ -1,4 +1,4 @@
-local M = {
+return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
@@ -33,40 +33,37 @@ local M = {
       "hrsh7th/cmp-nvim-lua",
     },
   },
-}
+  config = function()
+    local cmp = require "cmp"
+    local luasnip = require "luasnip"
+    require("luasnip/loaders/from_vscode").lazy_load()
 
-function M.config()
-  local cmp = require "cmp"
-  local luasnip = require "luasnip"
-  require("luasnip/loaders/from_vscode").lazy_load()
-
-  cmp.setup {
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body)
-      end,
-    },
-    mapping = cmp.mapping.preset.insert {
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm { select = true },
-    },
-    sources = {
-      { name = "nvim_lsp" },
-      { name = "nvim_lua" },
-      { name = "luasnip" },
-      { name = "buffer" },
-      { name = "path" },
-    },
-    window = {
-      completion = {
-        scrollbar = false,
-        winhighlight = "Normal:NormalFloat,FloatBorder:CmpNormal",
+    cmp.setup {
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
       },
-    },
-  }
-end
-
-return M
+      mapping = cmp.mapping.preset.insert {
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm { select = true },
+      },
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      },
+      window = {
+        completion = {
+          scrollbar = false,
+          winhighlight = "Normal:NormalFloat,FloatBorder:CmpNormal",
+        },
+      },
+    }
+  end,
+}
--- neovim/.config/nvim/lua/user/none-ls.lua
+++ neovim/.config/nvim/lua/user/none-ls.lua
@@ -1,22 +1,20 @@
-local M = {
+return {
   "nvimtools/none-ls.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  event = "VeryLazy",
+  config = function()
+    local null_ls = require "null-ls"
+    local formatting = null_ls.builtins.formatting
+
+    null_ls.setup {
+      debug = false,
+      sources = {
+        formatting.stylua,
+        formatting.prettier,
+        null_ls.builtins.completion.spell,
+      },
+    }
+  end,
 }
-
-function M.config()
-  local null_ls = require "null-ls"
-  local formatting = null_ls.builtins.formatting
-
-  null_ls.setup {
-    debug = false,
-    sources = {
-      formatting.stylua,
-      formatting.prettier,
-      null_ls.builtins.completion.spell,
-    },
-  }
-end
-
-return M
