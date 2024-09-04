e7ceb075c83addd838d3cbcb6af7981798f9ffd5
Kengo Suzuki <git@kengo.dev>
2024-09-04 13:40:16 +0900

    Update lualine config

--- neovim/.config/nvim/lua/user/lualine.lua
+++ neovim/.config/nvim/lua/user/lualine.lua
@@ -1,27 +1,19 @@
-local M = {
+return {
   "nvim-lualine/lualine.nvim",
-  dependencies = {
-    "AndreM222/copilot-lualine",
-  },
-}
-
-function M.config()
-  require("lualine").setup {
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
     options = {
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
       ignore_focus = { "NvimTree" },
     },
     sections = {
-      lualine_a = {},
+      lualine_a = { "mode" },
       lualine_b = { "branch" },
       lualine_c = { "diagnostics" },
-      lualine_x = { "copilot", "filetype" },
+      lualine_x = { "filetype" },
       lualine_y = { "progress" },
       lualine_z = {},
     },
-    extensions = { "quickfix", "man", "fugitive" },
-  }
-end
-
-return M
+  },
+}
