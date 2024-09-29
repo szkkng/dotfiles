b1bbc176a467eaf1fdd827221501b7c20091859f
Kengo Suzuki <git@kengo.dev>
2024-09-29 15:21:51 +0900

    Migrate to git.nix

---
+++ modules/git.nix
@@ -0,0 +1,33 @@
+{ ... }:
+{
+  programs.git = {
+    enable = true;
+    userName = "szkkng";
+    userEmail = "hi@kengo.dev";
+    aliases = {
+      st = "status";
+      ci = "commit";
+      co = "checkout";
+      sw = "switch";
+      last = "log - 1";
+      logol = "log - -oneline";
+    };
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+      fetch = {
+        prune = "true";
+      };
+      core = {
+        editor = "nvim";
+        autocrlf = "input";
+        ignorecase = "false";
+      };
+      pull = {
+        ff = "only";
+      };
+    };
+  };
+}
+
