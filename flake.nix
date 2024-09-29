6120044404ee14bd44bd31ed2249249e970ddfa3
szkkng <hi@kengo.dev>
2024-09-29 19:58:38 +0900

    Clean up

--- flake.nix
+++ flake.nix
@@ -20,72 +20,11 @@
       nix-darwin,
       home-manager,
     }:
-    let
-      configuration =
-        {
-          pkgs,
-          ...
-        }:
-        {
-          nixpkgs.config.allowUnfree = true;
-          nixpkgs.hostPlatform = "aarch64-darwin";
-          services.nix-daemon.enable = true;
-          nix.package = pkgs.nix;
-          nix.settings.experimental-features = "nix-command flakes";
-          system.stateVersion = 5;
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-
-          users.users.kengo = {
-            name = "kengo";
-            home = "/Users/kengo";
-          };
-
-          homebrew = {
-            enable = true;
-            casks = [
-              "1password"
-              "discord"
-              "font-jetbrains-mono-nerd-font"
-              "wezterm"
-              "keycastr"
-              "raycast"
-            ];
-            masApps = {
-              "LINE" = 539883307;
-              "Logic Pro" = 634148309;
-              "Magnet" = 441258766;
-              "Spark" = 1176895641;
-              "Klack" = 6446206067;
-            };
-          };
-
-          system.defaults = {
-            # https://macos-defaults.com/
-            dock = {
-              autohide = true;
-              show-recents = false;
-              mineffect = "scale";
-              static-only = true;
-            };
-            finder = {
-              AppleShowAllExtensions = true;
-              CreateDesktop = false;
-              ShowPathbar = true;
-              ShowStatusBar = true;
-            };
-            screencapture = {
-              location = "~/screenshots";
-            };
-            ".GlobalPreferences" = {
-              "com.apple.mouse.scaling" = 2.0;
-            };
-          };
-        };
-    in
     {
       darwinConfigurations."kengo-macbook-pro" = nix-darwin.lib.darwinSystem {
+        specialArgs = inputs;
         modules = [
-          configuration
+          ./modules/darwin.nix
           home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
