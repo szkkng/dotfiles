98b61de9f7fadcd9be39f660ec829374dde77b3a
szkkng <hi@kengo.dev>
2024-10-02 20:06:52 +0900

    Add options.nix

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -4,7 +4,14 @@
     enable = true;
     colorschemes.catppuccin.enable = true;
 
+    globals = {
+      # disable netrw for nvim-tree (strongly advised)
+      loaded_netrw = 1;
+      loaded_netrwPlugin = 1;
+    };
+
     imports = [
+      ./core/options.nix
       ./plugin/lualine.nix
     ];
   };
