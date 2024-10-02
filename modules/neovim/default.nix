4d861854bdcc077b68dae2648e468379d753bffe
Kengo Suzuki <git@kengo.dev>
2024-10-02 20:20:22 +0900

    Add keymaps.nix

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -8,10 +8,12 @@
       # disable netrw for nvim-tree (strongly advised)
       loaded_netrw = 1;
       loaded_netrwPlugin = 1;
+      mapleader = " ";
     };
 
     imports = [
       ./core/options.nix
+      ./core/keymaps.nix
       ./plugin/lualine.nix
     ];
   };
