49983952915bc77eb6a2d93c8bf60af6f50bec42
Kengo Suzuki <git@kengo.dev>
2024-10-14 13:48:18 +0900

    Add keymap for ClangdSwitchSourceHeader

--- neovim/.config/nvim/lua/plugin/server/clangd.lua
+++ neovim/.config/nvim/lua/plugin/server/clangd.lua
@@ -4,5 +4,8 @@ return {
     "--offset-encoding=utf-16",
     "--completion-style=detailed",
   },
+  on_attach = function()
+    vim.keymap.set("n", "gs", "<cmd>ClangdSwitchSourceHeader<CR>")
+  end,
 }
 
