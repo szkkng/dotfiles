8887b6be9bbd8ce424ecdf829e085e734f732a9c
szkkng <hi@kengo.dev>
2024-10-20 12:34:14 +0900

    Replace none-ls with conform and nvim-lint

---
+++ neovim/.config/nvim/lua/plugin/nvim-lint.lua
@@ -0,0 +1,18 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lint = require("lint")
+    lint.linters_by_ft = {
+      -- cmake = { "cmakelint" },
+    }
+
+    vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile", "InsertLeave" }, {
+      group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
+}
+
