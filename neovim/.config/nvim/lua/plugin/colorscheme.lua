d6aac10c9ddb274567f181ce77bc498cf91dff13
Kengo Suzuki <git@kengo.dev>
2024-11-17 23:04:48 +0900

    Replace hlchunk with ibl

--- neovim/.config/nvim/lua/plugin/colorscheme.lua
+++ neovim/.config/nvim/lua/plugin/colorscheme.lua
@@ -2,7 +2,15 @@ return {
   "catppuccin/nvim",
   lazy = false,
   priority = 1000,
-  config = function()
+  opts = {
+    integrations = {
+      indent_blankline = {
+        scope_color = "lavender",
+      },
+    },
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
     vim.cmd.colorscheme("catppuccin")
   end,
 }
