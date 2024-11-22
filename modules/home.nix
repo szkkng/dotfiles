a7ee784891a9c231c358d210e4c7290438ac504d
szkkng <hi@kengo.dev>
2024-11-22 14:17:45 +0900

    Prefer nixpkgs

--- modules/home.nix
+++ modules/home.nix
@@ -4,10 +4,17 @@
   home.stateVersion = "23.05";
   programs.home-manager.enable = true;
   home.packages = with pkgs; [
+    arc-browser
     cmake
     delta
+    discord
     gnused
+    keycastr
     ninja
+    obsidian
+    raycast
+    reaper
+    spotify
     supabase-cli
     karabiner-elements
   ];
