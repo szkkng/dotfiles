f9cdea49e33e9adbe958d6376dc5017d035e982f
szkkng <hi@kengo.dev>
2024-11-28 21:16:11 +0900

    Use conform format-on-save autocmd

--- modules/neovim/config/lua/core/autocmds.lua
+++ modules/neovim/config/lua/core/autocmds.lua
@@ -20,32 +20,9 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
   end,
 })
 
--- Format on save
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = augroup("format_on_save"),
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
-})
-
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   group = augroup("try_lint"),
   callback = function()
     require("lint").try_lint()
   end,
 })
-
--- Insert final newline
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = augroup("insert_final_newline"),
-  pattern = "*",
-  callback = function()
-    local num_lines = vim.api.nvim_buf_line_count(0)
-    local last_nonblank = vim.fn.prevnonblank(num_lines)
-    if last_nonblank <= num_lines then
-      vim.api.nvim_buf_set_lines(0, last_nonblank, num_lines, true, { "" })
-    end
-  end,
-})
-
