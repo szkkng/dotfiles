649361cc4302fac067118af6ad76b81a92edecb4
Kengo Suzuki <git@kengo.dev>
2024-11-29 12:29:02 +0900

    Clean up

--- modules/wezterm/default.nix
+++ modules/wezterm/default.nix
@@ -15,7 +15,6 @@
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/modules/wezterm/config";
       recursive = true;
     };
-    # https://github.com/nix-community/home-manager/issues/1807#issuecomment-1740960646
     "wezterm/wezterm.lua".enable = false;
   };
 }
