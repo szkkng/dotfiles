d6f34daf76b0fc5e5bec528666bb4bd71cd373ec
szkkng <hi@kengo.dev>
2024-11-29 20:28:13 +0900

    Remove lspsaga

--- modules/neovim/config/lua/plugin/lspconfig.lua
+++ modules/neovim/config/lua/plugin/lspconfig.lua
@@ -7,6 +7,31 @@ return {
     },
   },
   config = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+      callback = function(event)
+        local keymap = function(mode, keys, func)
+          vim.keymap.set(mode, keys, func, { buffer = event.buf })
+        end
+
+        keymap("n", "gd", require("telescope.builtin").lsp_definitions)
+        keymap("n", "gD", vim.lsp.buf.declaration)
+        keymap("n", "gt", require("telescope.builtin").lsp_type_definitions)
+        keymap("n", "gr", require("telescope.builtin").lsp_references)
+        keymap("n", "gI", require("telescope.builtin").lsp_implementations)
+        keymap("n", "gS", require("telescope.builtin").lsp_document_symbols)
+        keymap("n", "<leader>lr", vim.lsp.buf.rename)
+        keymap("n", "<leader>la", vim.lsp.buf.code_action)
+
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+          keymap("n", "<leader>lh", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+          end)
+        end
+      end,
+    })
+
     local servers = {
       "clangd",
       "lua_ls",
