f4277e8fda2fe10a89cce7794d1d0346ff77e166
Kengo Suzuki <git@kengo.dev>
2024-12-05 20:14:06 +0900

    Update clangd setting

--- modules/neovim/config/lua/plugin/server/clangd.lua
+++ modules/neovim/config/lua/plugin/server/clangd.lua
@@ -1,9 +1,4 @@
 return {
-  cmd = {
-    "clangd",
-    "--offset-encoding=utf-16",
-    "--completion-style=detailed",
-  },
   on_attach = function()
     vim.keymap.set("n", "gs", "<cmd>ClangdSwitchSourceHeader<CR>")
   end,
