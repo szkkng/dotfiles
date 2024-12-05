b01e3857a558c0cad904a001836e380c84fa16be
szkkng <hi@kengo.dev>
2024-12-05 14:05:47 +0900

    Add lazygit

---
+++ modules/lazygit.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "delta --dark --paging=never";
+        };
+      };
+      gui = {
+        nerdFontsVersion = "3";
+      };
+    };
+  };
+}
