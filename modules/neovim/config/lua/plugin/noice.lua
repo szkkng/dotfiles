6607a959d69a0058ca531f5e8d2838b7cd16a0fe
Kengo Suzuki <git@kengo.dev>
2024-12-06 14:06:38 +0900

    Enable bottom_search

--- modules/neovim/config/lua/plugin/noice.lua
+++ modules/neovim/config/lua/plugin/noice.lua
@@ -12,7 +12,7 @@ return {
       },
     },
     presets = {
-      -- bottom_search = true,
+      bottom_search = true,
       -- command_palette = true,
       long_message_to_split = true,
       inc_rename = true,
