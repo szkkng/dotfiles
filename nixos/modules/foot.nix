d13a85f8fc1b933814291e21579f5abfa816cf49
szkkng <hi@kengo.dev>
2025-02-25 17:33:41 +0900

    Decrease font size

--- nixos/modules/foot.nix
+++ nixos/modules/foot.nix
@@ -5,7 +5,7 @@
     settings = {
       main = {
         shell = "fish -c 'tmux a -t 0 || tmux'";
-        font = "JetBrains Mono Nerd Font:size=12";
+        font = "JetBrains Mono Nerd Font:size=10";
         dpi-aware = "yes";
         pad = "6x6";
       };
