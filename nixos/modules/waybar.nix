4e8a8b0b142f1c833d9ae68adb15527245edfd9c
szkkng <hi@kengo.dev>
2025-02-25 17:33:22 +0900

    Simplify waybar

--- nixos/modules/waybar.nix
+++ nixos/modules/waybar.nix
@@ -7,47 +7,23 @@
       mainBar = {
         layer = "top";
         position = "top";
-        height = 30;
+        # height = 20;
         spacing = 4;
         modules-left = [ "hyprland/workspaces" ];
         modules-center = [ "clock" ];
         modules-right = [
           "network"
-          "cpu"
-          "memory"
           "battery"
-          "tray"
         ];
         "hyprland/workspaces" = {
           format = "{name}";
           on-click = "activate";
         };
-        clock = {
-          format = "{:%I:%M %p}";
-        };
         network = {
-          format-wifi = "WiFi {signalStrength}%";
-          format-ethernet = "ETH";
-          format-disconnected = "Disconnected";
-        };
-        cpu = {
-          format = "CPU {usage}%";
-        };
-        memory = {
-          format = "MEM {usage}%";
+          format-wifi = "W {signalStrength}%";
         };
         battery = {
-          format = "{capacity}% {icon}";
-          format-icons = [
-            ""
-            ""
-            ""
-            ""
-            ""
-          ];
-        };
-        clock = {
-          format-alt = "{:%a, %d. %b  %H:%M}";
+          format = "P {capacity}%";
         };
       };
     };
@@ -56,8 +32,7 @@
         color: @text;
       }
       window#waybar {
-        background-color: shade(@base, 0.9);
-        border: 2px solid alpha(@crust, 0.3);
+        background-color: @base;
       }
     '';
   };
