2bc73464374fc849aaf1523c8593bf947d664d7d
szkkng <hi@kengo.dev>
2025-02-28 15:11:12 +0900

    Add tuxedo-rs

--- hosts/tuxedo-gen9/default.nix
+++ hosts/tuxedo-gen9/default.nix
@@ -179,6 +179,11 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
+  hardware.tuxedo-rs = {
+    enable = true;
+    tailor-gui.enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
