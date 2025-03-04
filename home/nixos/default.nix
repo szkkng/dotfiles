0e659da4f96fafbe85e95741c162fbe363bc4e1d
Kengo Suzuki <git@kengo.dev>
2025-03-04 19:48:56 +0900

    Fix nixos-rebuild errors

--- home/nixos/default.nix
+++ home/nixos/default.nix
@@ -2,13 +2,13 @@
 {
   imports = [
     ./../common
+    ./neovim
     ./cursor.nix
     ./fish.nix
     ./foot.nix
     ./gtk.nix
     ./home.nix
     ./hyprland.nix
-    ./musni.nix
     ./rofi.nix
     ./tmux.nix
     ./waybar.nix
