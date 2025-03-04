d53ca230bcb21226d5eebb2ab367435bb12207cc
szkkng <hi@kengo.dev>
2025-03-04 20:31:27 +0900

    Clean up

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./../common
@@ -7,6 +7,22 @@
     ./neovim
     ./wezterm
     ./fish.nix
-    ./home.nix
+  ];
+
+  home.username = "kengo";
+  home.stateVersion = "23.05";
+  programs.home-manager.enable = true;
+  home.packages = with pkgs; [
+    act
+    brave
+    bun
+    cmake
+    cargo
+    delta
+    discord
+    gnused
+    ninja
+    supabase-cli
+    spotify
   ];
 }
--- home/nixos/default.nix
+++ home/nixos/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./../common
@@ -7,9 +7,29 @@
     ./fish.nix
     ./foot.nix
     ./gtk.nix
-    ./home.nix
     ./hyprland.nix
     ./rofi.nix
     ./waybar.nix
+  ];
+
+  home.username = "kengo";
+  home.stateVersion = "23.05";
+  programs.home-manager.enable = true;
+  home.packages = with pkgs; [
+    _1password-gui
+    act
+    brave
+    brightnessctl
+    bun
+    cmake
+    cargo
+    delta
+    discord
+    gnused
+    ninja
+    pavucontrol
+    supabase-cli
+    spotify
+    xdg-desktop-portal-gtk
   ];
 }
