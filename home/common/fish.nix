1376cd4e40d5e1a75c383de317eaef0a3e32c62f
szkkng <hi@kengo.dev>
2025-03-18 16:15:51 +0900

    Clean up

--- home/common/fish.nix
+++ home/common/fish.nix
@@ -4,12 +4,6 @@
     enable = true;
     interactiveShellInit = ''
       set fish_greeting
-      fish_add_path $HOME/.scripts
-      fish_add_path $HOME/.ghcup/bin
-    '';
-    shellInitLast = ''
-      set -gx VOLTA_HOME "$HOME/.volta"
-      set -gx PATH "$VOLTA_HOME/bin" $PATH
     '';
     plugins = [
       {
