8fc8aa3639ff19a3ee7b66caa17692f6e920fcc8
szkkng <hi@kengo.dev>
2025-03-19 00:30:20 +0900

    Update diagnostics settings

--- home/common/nixvim/default.nix
+++ home/common/nixvim/default.nix
@@ -25,8 +25,22 @@
     };
 
     diagnostics = {
+      signs = {
+        text = {
+          "__rawKey__vim.diagnostic.severity.ERROR" = "";
+          "__rawKey__vim.diagnostic.severity.WARN" = "";
+          "__rawKey__vim.diagnostic.severity.HINT" = "󰌶";
+          "__rawKey__vim.diagnostic.severity.INFO" = "";
+        };
+      };
       virtual_text = false;
+      update_in_insert = false;
       severity_sort = true;
+      float = {
+        border = "rounded";
+        header = "";
+        prefix = "";
+      };
     };
 
     imports = [
