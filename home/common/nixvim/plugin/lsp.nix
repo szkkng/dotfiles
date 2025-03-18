77ed1b575bd7fa19c863d03d346f2d3ec6b3190e
Kengo Suzuki <git@kengo.dev>
2025-03-19 00:29:47 +0900

    Add keymaps for lsp

--- home/common/nixvim/plugin/lsp.nix
+++ home/common/nixvim/plugin/lsp.nix
@@ -2,6 +2,27 @@
 {
   plugins.lsp = {
     enable = true;
+    onAttach = # lua
+      ''
+        local keymap = function(mode, keys, func)
+          vim.keymap.set(mode, keys, func, { buffer = bufnr })
+        end
+
+        keymap("n", "gd", require("telescope.builtin").lsp_definitions)
+        keymap("n", "gD", vim.lsp.buf.declaration)
+        keymap("n", "gt", require("telescope.builtin").lsp_type_definitions)
+        keymap("n", "gr", require("telescope.builtin").lsp_references)
+        keymap("n", "gI", require("telescope.builtin").lsp_implementations)
+        keymap("n", "gS", require("telescope.builtin").lsp_document_symbols)
+        keymap("n", "<leader>la", vim.lsp.buf.code_action)
+
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+          keymap("n", "<leader>lh", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
+          end)
+        end
+      '';
+
     servers = {
       clangd = {
         enable = true;
@@ -11,7 +32,7 @@
           "--completion-style=detailed"
         ];
         onAttach.function = ''
-          vim.keymap.set("n", "<leader>gs", "<cmd>ClangdSwitchSourceHeader<CR>", {
+          vim.keymap.set("n", "gs", "<cmd>ClangdSwitchSourceHeader<CR>", {
             buffer = bufnr})
         '';
       };
