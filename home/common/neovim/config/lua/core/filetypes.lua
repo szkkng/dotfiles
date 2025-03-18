5bdb2d9f307030adbe1605a5918713517d6efefe
szkkng <hi@kengo.dev>
2025-03-19 01:19:32 +0900

    Remove old neovim config

--- home/common/neovim/config/lua/core/filetypes.lua
+++
@@ -1,9 +0,0 @@
-vim.filetype.add({
-  extension = {
-    gen = "cpp",
-  },
-  pattern = {
-    [".*/.github/workflows/.*%.yml"] = "yaml.ghaction",
-    [".*/.github/workflows/.*%.yaml"] = "yaml.ghaction",
-  },
-})
