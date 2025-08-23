5783ae13cde431a5920716e3293d1967435fba84
Kengo Suzuki <git@kengo.dev>
2025-08-23 13:28:14 +0900

    Disable send_composed_key

--- home/darwin/wezterm.nix
+++ home/darwin/wezterm.nix
@@ -29,6 +29,8 @@
           },
 
           scrollback_lines = 1000,
+          send_composed_key_when_left_alt_is_pressed = false,
+          send_composed_key_when_right_alt_is_pressed = false,
         }
       '';
   };
