dfeeabb4698a3fe6606998d33738b9132651078d
szkkng <hi@kengo.dev>
2025-10-20 14:22:48 +0900

    Remove model choices

--- home/common/neovim/config/lua/plugin/codecompanion/init.lua
+++ home/common/neovim/config/lua/plugin/codecompanion/init.lua
@@ -39,10 +39,6 @@ return {
             schema = {
               model = {
                 default = "gpt-5",
-                choices = {
-                  "gpt-5",
-                  "gpt-5-mini",
-                },
               },
             },
           })
