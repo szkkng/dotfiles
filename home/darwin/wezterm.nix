99a0fa1aa54ed23868cfa00d48bfff4c9a500801
szkkng <hi@kengo.dev>
2025-11-16 13:43:42 +0900

    Add window background blur

--- home/darwin/wezterm.nix
+++ home/darwin/wezterm.nix
@@ -19,7 +19,8 @@
 
           adjust_window_size_when_changing_font_size = false,
           window_close_confirmation = "NeverPrompt",
-          window_background_opacity = 1.0,
+          window_background_opacity = 0.9,
+          macos_window_background_blur = 20,
           window_decorations = "RESIZE",
           window_padding = {
             left = 10,
